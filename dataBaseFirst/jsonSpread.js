5f48504a066a4f531e65a3255b488bbece81fb00
MRolfe23 <31248765+MRolfe23@users.noreply.github.com>
2017-09-14 17:05:31 -0500

    Add files via upload
    one file is not needed

---
+++ dataBaseFirst/jsonSpread.js
@@ -0,0 +1,146 @@
+var data = [
+  {
+    "Name": "jim",
+    "email": "jim@gmail.com",
+    "age": 23,
+    "city": "minneapolis"
+  },
+  {
+    "Name": "brittney",
+    "email": "brit@gmail.com",
+    "age": 24,
+    "city": "Minneapolis"
+  },
+  {
+    "Name": "tim",
+    "email": "tim@gmail.com",
+    "age": 25,
+    "city": "miami"
+  },
+  {
+    "Name": "gail",
+    "email": "gail@gmail.com",
+    "age": 26,
+    "city": "seattle"
+  },
+  {
+    "Name": "ben",
+    "email": "ben@gmail.com",
+    "age": 27,
+    "city": "Los Andgeles"
+  },
+  {
+    "Name": "heather",
+    "email": "heather@gmail.com",
+    "age": 23,
+    "city": "new york"
+  },
+  {
+    "Name": "mike",
+    "email": "mike@gmail.com",
+    "age": 26,
+    "city": "Minneapolis"
+  },
+  {
+    "Name": "Liv",
+    "email": "liv@gmail.com",
+    "age": 22,
+    "city": "Edina"
+  },
+  {
+    "Name": "jeff",
+    "email": "jeff@gmail.com",
+    "age": 28,
+    "city": "North Field"
+  },
+  {
+    "Name": "jen",
+    "email": "jen@gmail.com",
+    "age": 24,
+    "city": "Carver"
+  },
+  {
+    "Name": "rick",
+    "email": "rick@gmail.com",
+    "age": 22,
+    "city": "scrantin"
+  },
+  {
+    "Name": "sophie",
+    "email": "sophie@gmail.com",
+    "age": 24,
+    "city": "Edina"
+  },
+  {
+    "Name": "Will",
+    "email": "will@gmail.com",
+    "age": 3,
+    "city": "wayzata"
+  },
+  {
+    "Name": "lauren",
+    "email": "lauren@gmail.com",
+    "age": 24,
+    "city": "Los Angekes"
+  },
+  {
+    "Name": "ken",
+    "email": "ken@gmail.com",
+    "age": 23,
+    "city": "st paul"
+  },
+  {
+    "Name": "lilly",
+    "email": "lilly@gmail.com",
+    "age": 38,
+    "city": "eden prairie"
+  },
+  {
+    "Name": "austin",
+    "email": "austin@gmail.com",
+    "age": 55,
+    "city": "placecity"
+  },
+  {
+    "Name": "donna",
+    "email": "donna@gmail.com",
+    "age": 87,
+    "city": "grave yard"
+  },
+  {
+    "Name": "harry",
+    "email": "harry@gmail.com",
+    "age": 19,
+    "city": "hogwarts"
+  },
+  {
+    "Name": "lisa",
+    "email": "LISA@GMAIL.COM",
+    "age": 12,
+    "city": "springfield"
+  },
+  {
+    "Name": "bart",
+    "email": "bart@gmail.com",
+    "age": 13,
+    "city": "springfield"
+  },
+  {
+    "Name": "mary",
+    "email": "mary@gmail.com",
+    "age": 27,
+    "city": "Cairns"
+  },
+  {
+    "Name": "lue",
+    "email": "lue@gmail.com",
+    "age": 21,
+    "city": "brisban"
+  },
+  {
+    "Name": "Rita",
+    "email": "rita@gmail.com",
+    "age": 29,
+    "city": "Cancun"
+  }
+]
